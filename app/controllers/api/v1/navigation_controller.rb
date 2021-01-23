328e5f6a088e6595e7e8c89d776966433e44ed21
limkoonkiat <lim.koonkiat42@gmail.com>
2021-01-23 18:57:39 +0800

    Extend search to tags of tasks

--- app/controllers/api/v1/navigation_controller.rb
+++ app/controllers/api/v1/navigation_controller.rb
@@ -7,6 +7,11 @@ class Api::V1::NavigationController < ApplicationController
     # Add tasks with matching name or description
     results = current_user.tasks.where('lower(name) LIKE ? OR lower(description) LIKE ?', "%#{query}%", "%#{query}%")
 
+    # Add tasks with matching tags
+    matching_tags = current_user.tags.where('lower(name) LIKE ?', "%#{query}%")
+    tasks_from_tags = matching_tags.flat_map{|tag| tag.tasks}
+    results += tasks_from_tags
+
     results = results.uniq
     results = results.map{|task| task.attributes.merge(tags: current_user.tasks.find(task.id).tags)} # Workaround for tasks not containing its tags
     render json: results
